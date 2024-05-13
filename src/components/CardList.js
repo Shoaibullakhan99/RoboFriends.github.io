9bd12e58d52da2502ee15db377ae2b6a51a324cd
Shoaib ulla khan <shoaibullakhan1999@outlook.com>
2024-05-13 16:21:59 +0530

    Updated the CardList component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,12 +3,12 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ Robots }) => {
-    const cardsArray = Robots.map((user, i) => {
+    const cardsArray = Robots.map((user, index) => {
         return (<Card
-            key={i}
-            id={Robots[i].id}
-            name={Robots[i].name}
-            email={Robots[i].email}
+            key={index}
+            id={Robots[index].id}
+            name={Robots[index].name}
+            email={Robots[index].email}
         />
         )
     })
